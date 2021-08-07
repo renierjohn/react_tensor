ab2f66351d111201de923645a77aec7798ee7ff6
Rendroid <renierjohnsediego@gmail.com>
2021-08-07 19:47:47 +0800

    udpate

---
+++ precache-manifest.8524e1563daad41ec011de9210318ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0548d372e9b9379526fdb57eb52b196d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71269ab0b554a3fd78c2",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "41004944d2674b899206",
+    "url": "/static/js/2.93de43fb.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.93de43fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71269ab0b554a3fd78c2",
+    "url": "/static/js/main.c0acbf79.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
